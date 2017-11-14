6843986d599e55ce556ce1a23cd6a8f264acd9de
nguyhuy <nguyhuy@pc5403k.lan.esiee.fr>
2017-11-14 10:43:42 +0100

    New Pieces

---
+++ TP2_Echecs/Echecs/Pion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP2_Echecs.IHM;
+
+namespace TP2_Echecs.Echecs
+{
+    class Pion : Piece
+    {
+        public Pion(Joueur joueur, TypePiece type) : base(joueur, type)
+        {
+        }
+
+        public override bool Deplacer(Case destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TP2_Echecs/Echecs/Roi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP2_Echecs.IHM;
+
+namespace TP2_Echecs.Echecs
+{
+    class Roi : Piece
+    {
+        public Roi(Joueur joueur, TypePiece type) : base(joueur, type)
+        {
+        }
+
+        public override bool Deplacer(Case destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
