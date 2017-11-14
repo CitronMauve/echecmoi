f5a071ecb0650aef591d07bffabc3214c7615ce8
giller <giller@pc5403j.lan.esiee.fr>
2017-11-14 11:12:21 +0100

    ajoute deplacement pion

--- TP2_Echecs/Echecs/Pion.cs
+++ TP2_Echecs/Echecs/Pion.cs
@@ -9,13 +9,32 @@ namespace TP2_Echecs.Echecs
 {
     class Pion : Piece
     {
+        public bool premierDeplacement = true;
         public Pion(Joueur joueur) : base(joueur, TypePiece.Pion)
         {
         }
 
         public override bool Deplacer(Case destination)
         {
-            throw new NotImplementedException();
+            if (destination.colonne != this.position.colonne) {
+                if (!premierDeplacement &&
+                    destination.rangee - this.position.rangee <= 2)
+                {
+                    premierDeplacement = false;
+                    return destination.pieceActuelle == null;
+                }
+                if (destination.rangee - this.position.rangee == 1)
+                {
+                    return destination.pieceActuelle == null;
+                }
+            }
+
+            if(destination.rangee - this.position.rangee == 1 &&
+                destination.colonne - this.position.colonne == 1)
+            {
+                return destination.pieceActuelle != null;
+            }
+            return false;
         }
     }
 }
