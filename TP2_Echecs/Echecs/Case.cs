e5b3f2c3213ae8ef47b6ad435ab8279f858f3ea5
nguyhuy <nguyhuy@pc5403k.lan.esiee.fr>
2017-11-14 09:22:20 +0100

    first commit

---
+++ TP2_Echecs/Echecs/Case.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP2_Echecs.IHM;
+
+namespace TP2_Echecs.Echecs
+{
+    public class Case
+    {
+        // attributs
+        
+
+        // associations
+        
+
+        // methodes
+        public void Link(Piece newPiece)
+        {
+            // 1. Deconnecter newPiece de l'ancienne case
+
+            // 2. Connecter newPiece à cette case
+        }
+    }
+}
---
+++ TP2_Echecs/Echecs/Joueur.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP2_Echecs.IHM;
+
+namespace TP2_Echecs.Echecs
+{
+    public class Joueur
+    {
+        // attributs
+        public CouleurCamp couleur;
+
+        // associations
+        public Partie partie;
+        public List<Piece> pieces = new List<Piece>();
+
+        // methodes
+        public Joueur(Partie partie, CouleurCamp couleur)
+        {
+            this.couleur = couleur;
+            this.partie = partie;
+
+            // TODO : creation des pieces du joueur
+            pieces.Add( new Dame(this) );
+        }
+
+        // TODO : décommentez lorsque vous auriez implementé les methode Unlink et Link de la classe Case
+        //public void PlacerPieces(Echiquier echiquier)
+        //{
+        //    if( couleur == CouleurCamp.Noire )
+        //    {
+        //        echiquier.cases[3, 0].Link( pieces[0] );
+        //    }
+        //    else
+        //    {
+        //        echiquier.cases[3, 7].Link( pieces[0] );
+        //    }
+        //}
+    }
+}
