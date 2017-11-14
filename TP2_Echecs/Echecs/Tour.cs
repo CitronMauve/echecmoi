bbcb4276d61441632fedeec534977c2e26c1b867
giller <giller@pc5403j.lan.esiee.fr>
2017-11-14 12:22:09 +0100

    ajoute deplacement tour

--- TP2_Echecs/Echecs/Tour.cs
+++ TP2_Echecs/Echecs/Tour.cs
@@ -15,7 +15,24 @@ namespace TP2_Echecs.Echecs
 
         public override bool Deplacer(Case destination)
         {
-            throw new NotImplementedException();
+            int diffColonne = destination.colonne - this.position.colonne;
+            int diffRangee = destination.rangee - this.position.rangee;
+            int i = 0;
+
+            if ((diffColonne == 0 && diffRangee != 0) ||
+                (diffColonne != 0 && diffRangee == 0))
+            {
+                while (Math.Abs(diffColonne) != i)
+                {
+                    if (joueur.partie.Echiquier.cases[i + this.position.rangee, i + this.position.colonne] != null)
+                    {
+                        return false;
+                    }
+                    ++i;
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
