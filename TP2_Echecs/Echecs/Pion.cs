17c99d2b562501932d3450811f93e57f8693a31f
NGUYEN Huy-Hai Vincent <nhhvincent@gmail.com>
2017-11-27 16:02:51 +0100

    Test formatting

--- TP2_Echecs/Echecs/Pion.cs
+++ TP2_Echecs/Echecs/Pion.cs
@@ -5,73 +5,55 @@ using System.Text;
 using System.Threading.Tasks;
 using TP2_Echecs.IHM;
 
-namespace TP2_Echecs.Echecs
-{
-    class Pion : Piece
-    {
-        public bool premierDeplacement = true;
-        public Pion(Joueur joueur) : base(joueur, TypePiece.Pion)
-        {
-        }
+namespace TP2_Echecs.Echecs {
+	class Pion : Piece {
+		public bool premierDeplacement = true;
 
-        public override bool Deplacer(Case destination)
-        {
-            if (DeplacerSurAllie(destination)) return false;
+		public Pion(Joueur joueur) : base(joueur, TypePiece.Pion) {
+		}
 
-            bool result = false;
+		public override bool Deplacer(Case destination) {
+			if (DeplacerSurAllie(destination)) return false;
+
+			bool result = false;
 
 			// White side
-			if (this.info.couleur == CouleurCamp.Blanche)
-			{
+			if (this.info.couleur == CouleurCamp.Blanche) {
 				// Check if first move, if so allow distance 2
 				if ((destination.colonne - this.position.colonne == -2 && premierDeplacement) ||
-					destination.colonne - this.position.colonne == -1)
-				{
+					destination.colonne - this.position.colonne == -1) {
 					// Check if an ennemi is present when moving from anoter column
-					/*
-					if ((Math.Abs(destination.rangee - this.position.rangee) == 1 && DeplacerSurEnnemi(destination)) ||
-						(destination.rangee - this.position.rangee == 0 && !DeplacerSurEnnemi(destination)))
-					{
-						result = true;
-					}
-					*/
 					// Normal movement expected from a Pion: no enemy
-					if (destination.rangee - this.position.rangee == 0 && !DeplacerSurEnnemi(destination))
-					{
+					if (destination.rangee - this.position.rangee == 0 && !DeplacerSurEnnemi(destination)) {
 						result = true;
-					// Movement with an enemy
-					} else if (Math.Abs(destination.rangee - this.position.rangee) == 1 && DeplacerSurEnnemi(destination))
-					{
+					} // Movement with an enemy
+					else if (Math.Abs(destination.rangee - this.position.rangee) == 1 && DeplacerSurEnnemi(destination)) {
 						result = true;
 					}
 				}
 			}
 			// Black side
-			else
-			{
+			else {
 				// Check if first move, if so allow distance 2
 				if ((destination.colonne - this.position.colonne == 2 && premierDeplacement) ||
-					destination.colonne - this.position.colonne == 1)
-				{
+					destination.colonne - this.position.colonne == 1) {
 					// Check if an ennemi is present when moving from anoter column
-					if ((Math.Abs(destination.rangee - this.position.rangee) == 1 && DeplacerSurEnnemi(destination)) ||
-						(destination.rangee - this.position.rangee == 0 && !DeplacerSurEnnemi(destination)))
-					{
+					// Normal movement expected from a Pion: no enemy
+					if (destination.rangee - this.position.rangee == 0 && !DeplacerSurEnnemi(destination)) {
+						result = true;
+					}
+					// Movement with an enemy
+					else if (Math.Abs(destination.rangee - this.position.rangee) == 1 && DeplacerSurEnnemi(destination)) {
 						result = true;
 					}
 				}
 			}
-			if (this.premierDeplacement && result)
-            {
+
+			if (this.premierDeplacement && result) {
 				this.premierDeplacement = false;
-				/*
-                destination.Link(this);
-                this.position.Unlink();
-				this.position = destination;
-				*/
 			}
-			
+
 			return result;
-        }
-    }
+		}
+	}
 }
