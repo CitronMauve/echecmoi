74388ddedaf51e1eb95632f557bb5be04e3fb696
NGUYEN Huy-Hai Vincent <nhhvincent@gmail.com>
2017-11-27 18:26:18 +0100

    Used Deplacer of Tour.cs and Fou.cs to make Deplacer of Dame.cs

--- TP2_Echecs/Echecs/Pieces/Dame.cs
+++ TP2_Echecs/Echecs/Pieces/Dame.cs
@@ -1,65 +1,95 @@
 ﻿using System;
 using TP2_Echecs.IHM;
 
-namespace TP2_Echecs.Echecs
-{
-    public class Dame : Piece
-    {
-        public Dame(Joueur joueur) : base(joueur, TypePiece.Dame) { }
+namespace TP2_Echecs.Echecs {
+	public class Dame : Piece {
+		public Dame(Joueur joueur) : base(joueur, TypePiece.Dame) { }
 
-        public override bool Deplacer(Case destination)
-        {
-            if (DeplacerSurAllie(destination)) return false;
+		public override bool Deplacer(Case destination) {
+			if (DeplacerSurAllie(destination)) return false;
 
-            bool result = true;
+			bool result = false;
+			
+			int diffColonne = destination.colonne - this.position.colonne;
+			int diffRangee = destination.rangee - this.position.rangee;
+			int rangeeToCheck = this.position.rangee;
+			int colonneToCheck = this.position.colonne;
+			int i = 0;
 
-          int diffColonne = destination.colonne - this.position.colonne;
-          int diffRangee = destination.rangee - this.position.rangee;
-          int i = 0;
+			//-------------------------------------------------------------------------------------------------------------------
+			// Movement of the Tour.cs
+			if (diffColonne == 0 && diffRangee != 0) {
+				rangeeToCheck = this.position.rangee;
+				do {
+					if (diffRangee < 0) {
+						--rangeeToCheck;
+					} else if (diffRangee > 0) {
+						++rangeeToCheck;
+					}
 
-          if (diffColonne == diffRangee)
-          {
-              while(Math.Abs(diffColonne) != i)
-              {
-                  if (joueur.partie.echiquier.cases[i + this.position.rangee, i + this.position.colonne] != null)
-                  {
-                      return false;
-                  }
-                  ++i;
-              }
-          }
+					result = joueur.partie.echiquier.cases[rangeeToCheck, this.position.colonne].pieceActuelle == null;
 
-          if (diffColonne != 0 && diffRangee == 0)
-          {
-              while (Math.Abs(diffColonne) != i)
-              {
-                  if (joueur.partie.echiquier.cases[this.position.rangee, i + this.position.colonne] != null)
-                  {
-                      return false;
-                  }
-                  ++i;
-              }
-          }
+					++i;
+				} while (result && i < Math.Abs(diffRangee));
 
-          if (diffColonne == 0 && diffRangee != 0)
-          {
-              while (Math.Abs(diffRangee) != i)
-              {
-                  if (joueur.partie.echiquier.cases[this.position.rangee, this.position.colonne] != null)
-                  {
-                      return false;
-                  }
-                  ++i;
-              }
-            }
+				// Check the case where the move is authorized but there is an enemy at the Destination
+				if (i == Math.Abs(diffRangee)) {
+					result = (null == joueur.partie.echiquier.cases[destination.rangee, destination.colonne].pieceActuelle || DeplacerSurEnnemi(destination));
+				}
+			} else if (diffColonne != 0 && diffRangee == 0) {
+				colonneToCheck = this.position.colonne;
+				do {
+					if (diffColonne < 0) {
+						--colonneToCheck;
 
-            if (result)
-            {
-                destination.Link(this);
-                this.position.Unlink();
-            }
+					} else if (diffColonne > 0) {
+						++colonneToCheck;
+					}
 
-            return result;
-        }
-    }
+					result = joueur.partie.echiquier.cases[this.position.rangee, colonneToCheck].pieceActuelle == null;
+
+					++i;
+				} while (result && i < Math.Abs(diffColonne));
+
+				// Check the case where the move is authorized but there is an enemy at the Destination
+				if (i == Math.Abs(diffColonne)) {
+					result = (null == joueur.partie.echiquier.cases[destination.rangee, destination.colonne].pieceActuelle || DeplacerSurEnnemi(destination));
+				}
+			}
+			// Movement of the Tour.cs
+			//-------------------------------------------------------------------------------------------------------------------
+
+			//-------------------------------------------------------------------------------------------------------------------
+			// Movement of the Fou.cs
+			if (Math.Abs(diffColonne) == Math.Abs(diffRangee)) {
+				do {
+					if (diffRangee < 0) {
+						--rangeeToCheck;
+					} else if (diffRangee > 0) {
+						++rangeeToCheck;
+					}
+
+					if (diffColonne < 0) {
+						--colonneToCheck;
+
+					} else if (diffColonne > 0) {
+						++colonneToCheck;
+					}
+
+					result = joueur.partie.echiquier.cases[rangeeToCheck, colonneToCheck].pieceActuelle == null;
+
+					++i;
+				} while (result && i < Math.Abs(diffColonne));
+
+				// Check the case where the move is authorized but there is an enemy at the Destination
+				if (i == Math.Abs(diffColonne)) {
+					result = (null == joueur.partie.echiquier.cases[destination.rangee, destination.colonne].pieceActuelle || DeplacerSurEnnemi(destination));
+				}
+			}
+			// Movement of the Fou.cs
+			//-------------------------------------------------------------------------------------------------------------------
+
+			return result;
+		}
+	}
 }
