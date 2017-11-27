2dda7adef4c84c549f1d22ccb38c4c3a6c0f86c4
NGUYEN Huy-Hai Vincent <nhhvincent@gmail.com>
2017-11-27 18:10:32 +0100

    Fixed Deplacer() of Cavalier

--- TP2_Echecs/Echecs/Pieces/Cavalier.cs
+++ TP2_Echecs/Echecs/Pieces/Cavalier.cs
@@ -5,30 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using TP2_Echecs.IHM;
 
-namespace TP2_Echecs.Echecs
-{
-    class Cavalier : Piece
-    {
-        public Cavalier(Joueur joueur) : base(joueur, TypePiece.Cavalier)
-        {
-        }
+namespace TP2_Echecs.Echecs {
+	class Cavalier : Piece {
+		public Cavalier(Joueur joueur) : base(joueur, TypePiece.Cavalier) {
+		}
 
-        public override bool Deplacer(Case destination)
-        {
-            if (DeplacerSurAllie(destination)) return false;
+		public override bool Deplacer(Case destination) {
+			if (DeplacerSurAllie(destination)) return false;
 
-            bool result = false;
+			bool result = false;
 
-            int diffColonne = Math.Abs(destination.colonne - this.position.colonne);
-            int diffRangee = Math.Abs(destination.rangee - this.position.rangee);
+			int diffColonne = Math.Abs(destination.colonne - this.position.colonne);
+			int diffRangee = Math.Abs(destination.rangee - this.position.rangee);
 
-            if (diffColonne == 2 && diffRangee == 1 ||
-                diffColonne == 1 && diffRangee == 2)
-            {
-                result = true;
-            }
+			if (diffColonne == 2 && diffRangee == 1 ||
+				diffColonne == 1 && diffRangee == 2) {
+				result = true;
+			}
 
-            return result;
-        }
-    }
+			return result;
+		}
+	}
 }
